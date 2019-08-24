e96fdd9e51d08b0ba07c77b77c7d45cf23bfeea5
Damien Senger <damien@raccoon.studio>
2019-08-23 21:28:20 -0400

    Setup Styled Components in this project

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #ffffff;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Message = styled.Text``;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Container>
+      <Message>Open up App.tsx to start working on your app!</Message>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
