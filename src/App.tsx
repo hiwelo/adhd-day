56704933a9defec2887511f30dd5576ae271c2f6
Damien Senger <damien@raccoon.studio>
2019-08-24 12:33:56 -0400

    --no-edit

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,13 @@ import { createAppContainer } from "react-navigation";
 
 import routes from './routes';
 import { store, persistor } from './store';
-import { HomeScreen } from './views';
 
 const AppContainer = createAppContainer(routes);
 
 const App = () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <HomeScreen />
+      <AppContainer />
     </PersistGate>
   </Provider>
 );
