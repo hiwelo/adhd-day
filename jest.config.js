4db6d793f363da5c3f3070009b0a4ae9be76be2c
Damien Senger <damien@raccoon.studio>
2019-08-30 23:57:58 +0200

    Install & Configure Jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'jest-expo',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|sentry-expo|native-base)',
+  ],
+};
