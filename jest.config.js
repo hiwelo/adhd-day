3c8a48b7db101188640b1d9398271d67f6055fc8
Damien Senger <damien@raccoon.studio>
2019-09-01 18:09:29 +0200

    Update ignored paths for Jest code coverage

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,6 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|sentry-expo|native-base)',
   ],
-  collectCoverageFrom: ['src/{common,data}/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  coveragePathIgnorePatterns: ['components/', 'node_modules/', 'views/'],
 };
